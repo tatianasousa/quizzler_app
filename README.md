5ad58a3216b2babd1d0768dcd0a69093ba342567
tatianasousa <tatianamariassousa@gmail.com>
2021-05-24 22:48:56 -0300

    quizzler app

---
+++ README.md
@@ -0,0 +1,4 @@
+# quizzler_app
+
+A new Flutter project.
+
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  late String questionText;
+  late String questionAnswer;
+
+  Question (String q, String a) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
